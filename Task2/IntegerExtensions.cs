afb34e33395abc071dc3fd1b9007f236161f4121
TwinkIeTwin <matyuha4@yandex.ru>
2020-01-15 13:27:55 +0300

    feat: GetGcd in IntegerExtensions

--- Task2/IntegerExtensions.cs
+++ Task2/IntegerExtensions.cs
@@ -6,8 +6,52 @@ namespace Task2
     {
         public static int GetGcd(int a, int b)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+            else if (a == 0)
+            {
+                return Math.Abs(b);
+            }
+            else if (b == 0)
+            {
+                return Math.Abs(a);
+            }
+            else
+            {
+                // use long because of potential overflow when a or b is max negative int
+                long absA = Math.Abs((long)a);
+                long absB = Math.Abs((long)b);
+                long k1, k2;
+                if (absA > absB)
+                {
+                    k1 = absA;
+                    k2 = absB;
+                }
+                else
+                {
+                    k1 = absB;
+                    k2 = absA;
+                }
+
+                long remainder;
+
+                // euclidean algorithm
+                while (true)
+                {
+                    remainder = k1 - ((k1 / k2) * k2);
+                    if (remainder == 0)
+                    {
+                        break;
+                    }
+
+                    k1 = k2;
+                    k2 = remainder;
+                }
+
+                return (int)k2;
+            }
         }
     }
 }
